5a2761b899fef028152300007941a2d6782a3c32
meomeo24dev <meomeo24dev@gmail.com>
2024-08-31 20:37:03 +0700

    feat : crud location

--- BackEnd/app/Http/Requests/StoresLocationRequest.php
+++ BackEnd/app/Http/Requests/StoresLocationRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateLocationRequest extends FormRequest
+class StoresLocationRequest extends FormRequest
 {
     public function authorize()
     {
@@ -14,8 +14,7 @@ class UpdateLocationRequest extends FormRequest
     public function rules()
     {
         return [
-            'location_name' => 'sometimes|string|max:255',
-
+            'location_name' => 'required|string|max:255',
         ];
     }
 }
