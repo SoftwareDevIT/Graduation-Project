b63ad64bfd4f0e151e13ebc61ba4dcb0bfbd1b89
Nguyen Thien <nguyenvanthien24032004@gmail.com>
2024-09-01 23:00:35 +0700

    docs:CinemaSelector

---
+++ Clinet/Du-an/src/Page/Bookcinematickets.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../component/Header/Hearder'
+import CinemaSelector from '../component/CinemaSelector/CinemaSelector'
+import Footer from '../component/Footer/Footer'
+
+interface Props {
+    
+}
+
+const Bookcinematickets = (props: Props) => {
+    return (
+        <div>
+            <Header/>
+            <CinemaSelector/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Bookcinematickets
