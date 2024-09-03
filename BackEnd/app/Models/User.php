4dffa0f414da590a59bb4fe128f1223f335af924
meomeo24dev <meomeo24dev@gmail.com>
2024-09-03 21:25:58 +0700

    move file

--- BackEnd/app/Models/User.php
+++ BackEnd/app/Models/User.php
@@ -6,16 +6,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
     use HasApiTokens, Notifiable, HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    use HasFactory, Notifiable;
+    protected $table = 'users';
     protected $fillable = [
         'user_id',
         'user_name',
@@ -31,23 +30,29 @@ class User extends Authenticatable
         'status',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function roles()
+    {
+        return $this->hasMany(Role::class, 'role_id', 'role_id');
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
+    public function setRoleIdAttribute($value)
+    {
+        $this->attributes['role_id'] = (int) $value;
+    }
 }
--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\Api\LocationApiController;
+use App\Http\Controllers\LocationApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
