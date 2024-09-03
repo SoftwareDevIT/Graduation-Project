69b0247ce05c331479bd683c4e8b5a0aeb2cec9e
meomeo24dev <meomeo24dev@gmail.com>
2024-09-03 21:48:22 +0700

    Update api.php

--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LocationApiController;
 use App\Http\Controllers\AccountVerificationController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
