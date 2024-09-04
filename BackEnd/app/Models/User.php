52a08632c498a52d0b4cb4a0ed12acc2119f97c5
meomeo24dev <meomeo24dev@gmail.com>
2024-09-04 22:28:27 +0700

    Update User.php

--- BackEnd/app/Models/User.php
+++ BackEnd/app/Models/User.php
@@ -22,7 +22,6 @@ class User extends Authenticatable
     use HasFactory,Notifiable;
     protected $table = 'users';
     protected $fillable = [
-        'user_id',
         'user_name',
         'sex',
         'password',
