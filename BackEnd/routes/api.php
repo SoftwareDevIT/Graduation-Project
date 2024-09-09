2195ac457f3b646b1bd480f07cfcb3d7419e3564
meomeo24dev <meomeo24dev@gmail.com>
2024-09-09 19:40:45 +0700

    Update api.php
    update route  use apiResource

--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -27,6 +27,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
 Route::get('/user', [AuthController::class, 'list']);
 
 Route::post('register', [AuthController::class, 'register']);
@@ -35,29 +36,14 @@ Route::get('/list', [AuthController::class, 'list']);
 Route::get('/verify-account/{userId}', [AccountVerificationController::class, 'verify'])->name('verify');
 
 
-Route::get('location', [LocationController::class, 'index']);
-Route::post('location', [LocationController::class, 'store']);
-Route::put('location/{id}', [LocationController::class, 'update']);
-Route::delete('location/{id}', [LocationController::class, 'destroy']);
+Route::apiResource('location', LocationController::class);
 
-Route::get('cinema', [CinemaController::class, 'index']);
-Route::get('cinema/{id}', [CinemaController::class, 'show']);
-Route::post('cinema', [CinemaController::class, 'store']);
-Route::put('cinema/{id}', [CinemaController::class, 'update']);
-Route::delete('cinema/{id}', [CinemaController::class, 'destroy']);
+Route::apiResource('cinema', CinemaController::class);
 
 Route::apiResource('actor', ActorController::class);
 Route::apiResource('director', DirectorController::class);
 Route::apiResource('movie-category', MovieCategoryController::class);
 
-Route::get('method', [PayMethodController::class, 'index']);
-Route::get('method/{id}', [PayMethodController::class, 'show']);
-Route::post('method', [PayMethodController::class, 'store']);
-Route::put('method/{id}', [PayMethodController::class, 'update']);
-Route::delete('method/{id}', [PayMethodController::class, 'destroy']);
+Route::apiResource('method', PayMethodController::class);
 
-Route::get('combo', [ComboController::class, 'index']);
-Route::get('combo/{id}', [ComboController::class, 'show']);
-Route::post('combo', [ComboController::class, 'store']);
-Route::put('combo/{id}', [ComboController::class, 'update']);
-Route::delete('combo/{id}', [ComboController::class, 'destroy']);
+Route::apiResource('combo', ComboController::class);
