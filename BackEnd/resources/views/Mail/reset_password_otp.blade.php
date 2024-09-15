07fa48fcb15150a8f7394f7a95d5132ca212df1e
Nguyen Dinh Ba <yuuri121804@gmail.com>
2024-09-15 19:07:04 +0700

    feat: reset Password

---
+++ BackEnd/resources/views/Mail/reset_password_otp.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your OTP Code for Password Reset</title>
+</head>
+<body>
+    <h1>Your OTP Code</h1>
+    <p>Here is your OTP code to reset your password:</p>
+    <h2>{{ $otp }}</h2>
+    <p>This code is valid for 5 minutes.</p>
+</body>
+</html>
