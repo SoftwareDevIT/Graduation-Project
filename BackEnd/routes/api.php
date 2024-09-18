8c75a75175478568b3e657032d7d6d6e24b43bca
meomeo24dev <meomeo24dev@gmail.com>
2024-09-18 13:00:39 +0700

    Update api.php

--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -31,11 +31,6 @@ use App\Http\Controllers\Api\PayMethod\PayMethodController;
 |
 */
 
-
-// Route::middleware('auth:sanctum')->get('user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::middleware('auth:sanctum')->get('user', function (Request $request) {
     return $request->user();
 });
