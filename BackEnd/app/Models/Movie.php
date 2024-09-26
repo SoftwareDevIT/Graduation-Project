34b4273c9466e8145e5c07f5670fa954ce6fd421
kien189 <kien18092004@gmail.com>
2024-09-26 23:06:45 +0700

    fix: database movies

--- BackEnd/app/Models/Movie.php
+++ BackEnd/app/Models/Movie.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     use HasFactory;
-    protected $table = 'movie';
+    protected $table = 'movies';
     protected $fillable = [
         'movie_category_id',
         'actor_id',
--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -31,9 +31,9 @@ use App\Http\Controllers\Api\Auth\AccountVerificationController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', [AuthController::class, 'list']);
     Route::post('favorites', [FavoriteController::class, 'store']);             // Chức năng yêu thích phim
@@ -64,8 +64,8 @@ Route::apiResource('cinema', CinemaController::class);
 
 
 
-Route::resource('news_category', NewCategoryController::class); // crud của danh mục tin tức
-Route::resource('news', NewController::class); // crud của tin tức
+Route::apiResource('news_category', NewCategoryController::class); // crud của danh mục tin tức
+Route::apiResource('news', NewController::class); // crud của tin tức
 
 
 Route::apiResource('actor', ActorController::class);                        // crud của tác giả
@@ -88,35 +88,17 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('cinema', CinemaController::class);
     Route::apiResource('room', RoomController::class);
     Route::apiResource('showtimes', ShowtimeController::class);
-    Route::get('showtimes/movie/{movie_name}', [ShowtimeController::class, 'showtimeByMovieName']);
-    Route::resource('news_category', NewCategoryController::class);
-    Route::resource('news', NewController::class);
+    Route::apiResource('news_category', NewCategoryController::class);
+    Route::apiResource('news', NewController::class);
     Route::apiResource('actor', ActorController::class);
     Route::apiResource('director', DirectorController::class);
     Route::apiResource('movie-category', MovieCategoryController::class);
     Route::apiResource('movies', MovieController::class);
     Route::apiResource('method', PayMethodController::class);
     Route::apiResource('combo', ComboController::class);
+    Route::get('showtimes/movie/{movie_name}', [ShowtimeController::class, 'showtimeByMovieName']);
 });
 
-// Route::middleware(['auth:sanctum'])->group(function () {
-//     Route::post('logout', [AuthController::class, 'logout']);
-//     Route::get('list', [AuthController::class, 'list']);// danh sách tài khoản user
-//     Route::apiResource('location', LocationController::class);
-//     Route::apiResource('user', AuthController::class);
-//     Route::apiResource('cinema', CinemaController::class);
-//     Route::apiResource('room', RoomController::class);
-//     Route::apiResource('showtimes', ShowtimeController::class);
-//     Route::get('showtimes/movie/{movie_name}', [ShowtimeController::class, 'showtimeByMovieName']);
-//     Route::resource('news_category', NewCategoryController::class);
-//     Route::resource('news', NewController::class);
-//     Route::apiResource('actor', ActorController::class);
-//     Route::apiResource('director', DirectorController::class);
-//     Route::apiResource('movie-category', MovieCategoryController::class);
-//     Route::apiResource('movies', MovieController::class);
-//     Route::apiResource('method', PayMethodController::class);
-//     Route::apiResource('combo', ComboController::class);
-// });
 
 Route::get('cenima/{id}', [CinemaController::class, 'filterMovie']);
 // Lọc phim theo rạp phim
