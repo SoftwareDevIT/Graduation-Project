e8e8d8fd18fa3303bee0f1a5fc4ab935918984cf
meomeo24dev <meomeo24dev@gmail.com>
2024-09-28 10:38:28 +0700

    Update login.blade.php

--- BackEnd/resources/views/auth/login.blade.php
+++ BackEnd/resources/views/auth/login.blade.php
@@ -27,7 +27,7 @@
         </div>
         <div class="form-group">
             <label for="password">Password:</label>
-            <input type="password" class="form-control" id="password" name="password" required>
+            <input type="password" class="form-control" value="password" id="password" name="password" required>
         </div>
         <button type="submit" class="btn btn-primary">Login</button>
     </form>
