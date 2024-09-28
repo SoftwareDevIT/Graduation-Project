dd84a7714e3e62ff5f7660487fd58f75d6230752
meomeo24dev <meomeo24dev@gmail.com>
2024-09-28 10:23:19 +0700

    Update DatabaseSeeder.php

--- BackEnd/database/seeders/DatabaseSeeder.php
+++ BackEnd/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,8 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
         $this->call([
-            RoleSeeder::class,
+                // RoleSeeder::class,
             LocationSeeder::class,
             CinemaSeeder::class,
             PayMethodSeeder::class,
@@ -30,5 +34,49 @@ class DatabaseSeeder extends Seeder
             CategorySeatSeeder::class,
             // SeatSeeder::class,
         ]);
+        // Tạo các quyền
+        $permissions = [
+            'index',
+            'show',
+            'create',
+            'store',
+            'edit',
+            'update',
+            'destroy'
+        ];
+
+        foreach ($permissions as $permission) {
+         // kiểm tra xem có phải sự cho phép đã tồn tại không, nếu không, hãy tạo nó
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        // Tạo vai trò và gán các quyền cho mỗi vai trò
+        $roles = [
+            'admin',
+            'manager',
+            'staff',
+            'user',
+        ];
+
+        foreach ($roles as $roleName) {
+          // kiểm tra xem vai trò đã tồn tại, nếu không, hãy tạo nó
+            $role = Role::firstOrCreate(['name' => $roleName]);
+
+            // Gán tất cả các quyền cho vai trò 'admin'
+            if ($roleName === 'admin') {
+                $role->givePermissionTo(Permission::all()); // Gán tất cả quyền cho vai trò admin
+            } else {
+                // Gán một vài quyền cho các vai trò khác (ví dụ: chỉ index, show)
+                $role->givePermissionTo(['index', 'show']);
+            }
+        }
+
+        // Gán vai trò 'admin' cho người dùng với ID 2
+        $user = User::find(1); // Giả sử người dùng có ID 2 tồn tại
+        if ($user) {
+            $user->assignRole('admin');
+        }else{
+            $user->assignRole('user');
+        }
     }
 }
