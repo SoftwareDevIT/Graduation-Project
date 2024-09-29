498493aa93e29ebedf62fb9678536c65b119f3e0
Nguyen Thien <nguyenvanthien24032004@gmail.com>
2024-09-29 16:38:35 +0700

    Add Movies

--- BackEnd/app/Http/Requests/Movie/Store/StoreMovieRequest.php
+++ BackEnd/app/Http/Requests/Movie/Store/StoreMovieRequest.php
@@ -27,7 +27,7 @@ class StoreMovieRequest extends FormRequest
             'actor_id'              => 'required|integer',
             'director_id'           => 'required|integer',
             'movie_name'            => 'required|string|max:255',
-            'poster'                => 'mimes:jpeg,png,jpg,gif|max:2048',
+            'poster'                => '',
             'duration'              => 'string|max:225',
             'release_date'          => 'date',
             'age_limit'             => 'required|integer',
