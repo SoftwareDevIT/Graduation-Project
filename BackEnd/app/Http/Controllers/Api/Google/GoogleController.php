a703adc436bdb47e72792c3e5fbd4b6338fd20c4
meomeo24dev <meomeo24dev@gmail.com>
2024-10-06 00:12:53 +0700

    fix : login google

--- BackEnd/app/Http/Controllers/Api/Google/GoogleController.php
+++ BackEnd/app/Http/Controllers/Api/Google/GoogleController.php
@@ -7,55 +7,85 @@ use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Http\Response;
 use App\Models\User;
-
+use App\Services\Auth\LoginService;
+use Illuminate\Support\Facades\Hash;
 
 class GoogleController extends Controller
 {
+    protected $loginService;
+
+    public function __construct(LoginService $loginService)
+    {
+        $this->loginService = $loginService;
+    }
+
     public function getGoogleSignInUrl()
     {
         try {
-            $url = Socialite::driver('google')->stateless()
-                ->redirect()->getTargetUrl();
-            return response()->json([
-                'url' => $url,
-            ])->setStatusCode(Response::HTTP_OK);
+            $url = Socialite::driver('google')->stateless()->redirect()->getTargetUrl();
+            return response()->json(['url' => $url], Response::HTTP_OK);
         } catch (\Exception $exception) {
-            return $exception;
+            return response()->json([
+                'status' => 'failed',
+                'message' => $exception->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
+    // Google Login Callback
     public function loginCallback(Request $request)
     {
         try {
-            $state = $request->input('state');
-
-            parse_str($state, $result);
             $googleUser = Socialite::driver('google')->stateless()->user();
 
+            // Check if the user already exists in the database
             $user = User::where('email', $googleUser->email)->first();
+
             if ($user) {
-                throw new \Exception(__('google sign in email existed'));
-            }
-            $user = User::create(
-                [
+                $credentials = [
+                    'email' => $user->email,
+                    'password' => 'password',
+                ];
+
+                $token = $this->loginService->login($credentials);
+                $profile = $this->loginService->index();
+                return $this->success(['token' => $token, 'profile' => $profile], 200);
+
+                // return response()->json([
+                //     'message' => __('Google login successful'),
+                //     'token' => $token,
+                //     'profile' => $profile
+                // ], Response::HTTP_OK);
+
+            } else {
+                $newUser = User::create([
                     'email' => $googleUser->email,
                     'name' => $googleUser->name,
-                    'google_id'=> $googleUser->id,
-                    'password'=> '123',
-                    'email_verified_at'=> '2024-10-05 23:00:59',
-                ]
-            );
-            return response()->json([
-                'status' => __('google sign in successful'),
-                'data' => $user,
-            ], Response::HTTP_CREATED);
+                    'google_id' => $googleUser->id,
+                    'password' => 'password',
+                    'email_verified_at' => now(),
+                ]);
+
+                $credentials = [
+                    'email' => $newUser->email,
+                    'password' => 'password',
+                ];
+
+                $token = $this->loginService->login($credentials);
+                return $this->success(['token' => $token, 'profile' => $newUser], 200);
+                // return response()->json([
+                //     'message' => __('Google sign in successful'),
+                //     'token' => $token,
+                //     'profile' => $newUser
+                // ], Response::HTTP_CREATED);
+            }
 
         } catch (\Exception $exception) {
             return response()->json([
-                'status' => __('google sign in failed'),
-                'error' => $exception,
-                'message' => $exception->getMessage()
+                'message' => __('Google sign in failed'),
+                'error' => $exception->getMessage()
             ], Response::HTTP_BAD_REQUEST);
         }
     }
+
 }
