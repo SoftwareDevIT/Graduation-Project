e45fa29682f0a6158f6b9e4b446605fe5a40dd9a
Nguyen Dinh Ba <yuuri121804@gmail.com>
2024-11-02 00:23:23 +0700

    fix: email order

--- BackEnd/app/Models/News.php
+++ BackEnd/app/Models/News.php
@@ -24,4 +24,9 @@ class News extends Model
     {
         return $this->belongsTo(NewsCategory::class, 'news_category_id');
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
