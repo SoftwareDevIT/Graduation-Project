b44036fe6679337d974632faefc0ec84726db9ca
kien189 <kien18092004@gmail.com>
2024-11-02 22:27:19 +0700

    refactor: update code

--- BackEnd/app/Services/Booking/TicketBookingService.php
+++ BackEnd/app/Services/Booking/TicketBookingService.php
@@ -82,7 +82,7 @@ class TicketBookingService
                     ->where('seat_column', $seat['seat_column'])
                     ->where('room_id', $seat['room_id'])
                     ->first();
-                $seatData[] = $seat;    
+                $seatData[] = $seat;
             }
             Session::put('seats', $seatData);
             $this->bookTicketSaveSession($request, $bookTicket);
@@ -101,7 +101,7 @@ class TicketBookingService
         } else {
             Log::warning('No combos found in session.');
         }
-    
+
 
         // Kiểm tra xem session có chứa seats không
         if (session()->has('seats') && session('seats')) {
@@ -130,7 +130,7 @@ class TicketBookingService
             $urlPayment = $this->processPaymentStep->vnpay($request);  // Gọi phương thức VNPAY
             return response()->json(['url' => $urlPayment]);
         } elseif ($request->pay_method_id == 2) {
-            $urlPayment = $this->processPaymentStep->momo($request);  // Gọi phương thức MOMO
+            $urlPayment = $this->processPaymentStep->momoPayment($request);  // Gọi phương thức MOMO
             return response()->json(['url' => $urlPayment]);
         } else {
             // Xóa booking nếu phương thức thanh toán không hợp lệ
