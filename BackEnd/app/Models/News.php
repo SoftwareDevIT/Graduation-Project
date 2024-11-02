027c28845d14b07b035a370950490a6ff7727652
Nguyen Dinh Ba <yuuri121804@gmail.com>
2024-11-02 22:59:58 +0700

    no commit

--- BackEnd/app/Models/News.php
+++ BackEnd/app/Models/News.php
@@ -28,7 +28,7 @@ class News extends Model
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
-
+    }
     public function movie()
     {
         return $this->belongsTo(Movie::class, 'movie_id');
