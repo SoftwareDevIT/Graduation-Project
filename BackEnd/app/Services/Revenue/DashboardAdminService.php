cdea8b69cc5de2a2318a5c205d4eec21d681f714
anhdung2282003 <trandung2282003@gmail.com>
2024-11-26 17:15:31 +0700

    update dashbord

---
+++ BackEnd/app/Services/Revenue/DashboardAdminService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Services\Revenue;
+
+use App\Models\Booking;
+use App\Models\Showtime;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Support\Facades\Log;
+
+
+class DashboardAdminService
+{
+
+    public function dashboardAdmin()
+    {
+
+        $query = Booking::query();
+
+        // Tải các mối quan hệ cần thiết
+        $bookings = $query->with('showtime.movieInCinema.movie')->get();
+
+        // Khởi tạo mảng kết quả
+        $result = [
+            'Pending' => [],
+            'Pain' => [],
+            'Confirmed' => []
+        ];
+
+        // Nhóm dữ liệu theo trạng thái
+        foreach ($bookings as $booking) {
+            $status = $booking->status; // Trạng thái của booking
+
+            // Kiểm tra xem trạng thái có trong mảng kết quả không
+            if (isset($result[$status])) {
+                // Đếm số lượng vé cho từng booking trong trạng thái này
+                $quantity_ticket = Booking::where('status', $status)->count();
+
+                // Thêm thông tin vào mảng kết quả
+                $result[$status][] = [
+                    'amount' => $booking->amount,
+                    'price_combo' => $booking->price_combo,
+                    'price_ticket' => $booking->price_ticket,
+                    'quantity_ticket' => $quantity_ticket // Số lượng booking cho trạng thái hiện tại
+                ];
+            }
+        }
+
+        return $result; // Trả về kết quả đã nhóm theo trạng thái
+
+    }
+}
