ba39d43e2ca2ac5dd28fb5fa8d7a9ff534184d83
Minh Anh <anhtmph41558@fpt.edu.vn>
2024-12-05 00:35:50 +0700

    update seeder

--- BackEnd/database/seeders/RoomSeeder.php
+++ BackEnd/database/seeders/RoomSeeder.php
@@ -22,7 +22,7 @@ class RoomSeeder extends Seeder
                 DB::table('room')->insert([
                     'room_name' => 'P' . $i,
                     'cinema_id' => $cinemaId,
-                    'seat_layout_id' => rand(1, 4),
+                    'seat_map_id' => rand(1, 2),
                     'status' => '1', // Active status
                 ]);
             }
