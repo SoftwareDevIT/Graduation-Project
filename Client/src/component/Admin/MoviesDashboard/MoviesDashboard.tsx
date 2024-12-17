16133025a320735cf50193ae8cddbff652cec33f
Minh Anh <anhtmph41558@fpt.edu.vn>
2024-12-17 14:24:26 +0700

    update

--- Client/src/component/Admin/MoviesDashboard/MoviesDashboard.tsx
+++ Client/src/component/Admin/MoviesDashboard/MoviesDashboard.tsx
@@ -12,8 +12,8 @@ const MoviesDashboard: React.FC = () => {
     const { movies } = state;
     const { Search } = Input;
     const [error, setError] = useState<string | null>(null);
-    const [searchTerm, setSearchTerm] = useState<string>(''); 
-    const [currentPage, setCurrentPage] = useState<number>(1); 
+    const [searchTerm, setSearchTerm] = useState<string>('');
+    const [currentPage, setCurrentPage] = useState<number>(1);
     const [moviesPerPage] = useState<number>(5);
 
     useEffect(() => {
@@ -29,7 +29,7 @@ const MoviesDashboard: React.FC = () => {
 
         fetchMovies();
     }, [dispatch]);
-    
+
 
     const deleteMovie = async (id: number) => {
         if (window.confirm('Bạn có chắc chắn muốn xóa phim này không?')) {
@@ -52,109 +52,113 @@ const MoviesDashboard: React.FC = () => {
         }
     };
 
-    const filteredMovies = Array.isArray(movies) 
-        ? movies.filter(movie => 
+    const filteredMovies = Array.isArray(movies)
+        ? movies.filter(movie =>
             movie.movie_name.toLowerCase().includes(searchTerm.toLowerCase())
-          ) 
+        )
         : [];
 
-        const updateMovieStatus = async (id: number, currentStatus: boolean) => {
-            try {
-                const newStatus = !currentStatus; // Đảo trạng thái hiện tại
-                await instance.post(`/manager/movieStatus/${id}`, { status: newStatus ? 1 : 0 });
-        
-                // Cập nhật trạng thái trên giao diện
-                dispatch({
-                    type: 'UPDATE_MOVIE_STATUS',
-                    payload: { id, status: newStatus ? 1 : 0 },
-                });
-        
-                notification.success({
-                    message: 'Cập Nhật Trạng Thái',
-                    description: `Phim đã được ${newStatus ? 'hiện' : 'ẩn'}.`,
-                    placement: 'topRight',
-                });
-            } catch (error) {
-                console.error('Lỗi khi cập nhật trạng thái phim:', error);
-                notification.error({
-                    message: 'Lỗi',
-                    description: 'Không thể cập nhật trạng thái phim.',
-                    placement: 'topRight',
-                });
-            }
-        };
-        
-const columns = [
-    {
-        title: 'ID',
-        dataIndex: 'id',
-        key: 'id',
-        className: 'text-center',
-    },
-    {
-        title: 'Ảnh',
-        dataIndex: 'poster',
-        key: 'poster',
-        render: (poster: string) => (
-            <img src={poster} alt="Poster" style={{ width: '100px', height: '150px', objectFit: 'cover' }} />
-        ),
-        className: 'text-center',
-    },
-    {
-        title: 'Thông Tin Phim',
-        key: 'movie_info',
-        render: (movie: Movie) => (
-            <div>
-                <p><strong>Tên phim :</strong> {movie.movie_name}</p>
-                <p><strong>Thời Lượng:</strong> {movie.duration} phút</p>
-                <p><strong>Giới Hạn Tuổi:</strong> {movie.age_limit}+</p>
-                <p><strong>Quốc gia:</strong> {movie.country}</p>
-                <p><strong>Ngày Công Chiếu:</strong> {movie.release_date}</p>
-                {/* <p><strong>Luợt xem :</strong> {movie.views}</p>
-                <p><strong>Đánh giá :</strong> {movie.rating}</p> */}
-            </div>
-        ),
-        className: 'text-left',
-    },
-    {
-        title: 'Trạng Thái',
-        key: 'status',
-        render: (movie: Movie) => (
-            <div style={{ textAlign: 'left' }}>
-                <Switch
-                    checkedChildren="On"
-                    unCheckedChildren="Off"
-                    checked={movie.status === 1} // Kiểm tra trạng thái hiện tại
-                    onChange={() => updateMovieStatus(movie.id, movie.status === 1)}
-                />
-            </div>
-        ),
-        className: 'text-left',
-    },
-    
-    {
-        title: 'Hành Động',
-        key: 'action',
-        render: (text: any, movie: any) => (
-            <div className="d-flex justify-content-around">
-                <Link to={`/admin/movies/edit/${movie.id}`}>
-                    <Button type="primary" icon={<EditOutlined />} />
-                </Link>
-                <Popconfirm
-                    title="Bạn có chắc chắn muốn xóa phim này?"
-                    onConfirm={() => deleteMovie(movie.id)}
-                    okText="Có"
-                    cancelText="Không"
-                >
-                    <Button danger icon={<DeleteOutlined />} />
-                </Popconfirm>
-            </div>
-        ),
-        className: 'text-center`',
-    },
-];
+    const updateMovieStatus = async (id: number, currentStatus: boolean) => {
+        try {
+            const newStatus = !currentStatus; // Đảo trạng thái hiện tại
+            await instance.post(`/manager/movieStatus/${id}`, { status: newStatus ? 1 : 0 });
 
-        
+            // Cập nhật trạng thái trên giao diện
+            dispatch({
+                type: 'UPDATE_MOVIE_STATUS',
+                payload: { id, status: newStatus ? 1 : 0 },
+            });
+
+            notification.success({
+                message: 'Cập Nhật Trạng Thái',
+                description: `Phim đã được ${newStatus ? 'hiện' : 'ẩn'}.`,
+                placement: 'topRight',
+            });
+        } catch (error) {
+            console.error('Lỗi khi cập nhật trạng thái phim:', error);
+            notification.error({
+                message: 'Lỗi',
+                description: 'Không thể cập nhật trạng thái phim.',
+                placement: 'topRight',
+            });
+        }
+    };
+
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id',
+            className: 'text-center',
+        },
+        {
+            title: 'Ảnh',
+            dataIndex: 'poster',
+            key: 'poster',
+            render: (poster: string) => (
+                <img src={poster} alt="Poster" style={{ width: '100px', height: '150px', objectFit: 'cover' }} />
+            ),
+            className: 'text-center',
+        },
+        {
+            title: 'Thông Tin Phim',
+            key: 'movie_info',
+            render: (movie: Movie) => (
+                <div>
+                    <p><strong>Tên phim :</strong> {movie.movie_name}</p>
+                    <p><strong>Thời Lượng:</strong> {movie.duration} phút</p>
+                    <p><strong>Giới Hạn Tuổi:</strong> {movie.age_limit}+</p>
+                    <p><strong>Quốc gia:</strong> {movie.country}</p>
+                    <p><strong>Ngày Công Chiếu:</strong> {new Intl.DateTimeFormat('vi-VN', {
+                        day: '2-digit',
+                        month: '2-digit',
+                        year: 'numeric',
+                    }).format(new Date(movie.release_date))}</p>
+                    {/* <p><strong>Luợt xem :</strong> {movie.views}</p>
+                <p><strong>Đánh giá :</strong> {movie.rating}</p> */}
+                </div>
+            ),
+            className: 'text-left',
+        },
+        {
+            title: 'Trạng Thái',
+            key: 'status',
+            render: (movie: Movie) => (
+                <div style={{ textAlign: 'left' }}>
+                    <Switch
+                        checkedChildren="On"
+                        unCheckedChildren="Off"
+                        checked={movie.status === 1} // Kiểm tra trạng thái hiện tại
+                        onChange={() => updateMovieStatus(movie.id, movie.status === 1)}
+                    />
+                </div>
+            ),
+            className: 'text-left',
+        },
+
+        {
+            title: 'Hành Động',
+            key: 'action',
+            render: (text: any, movie: any) => (
+                <div className="d-flex justify-content-around">
+                    <Link to={`/admin/movies/edit/${movie.id}`}>
+                        <Button type="primary" icon={<EditOutlined />} />
+                    </Link>
+                    <Popconfirm
+                        title="Bạn có chắc chắn muốn xóa phim này?"
+                        onConfirm={() => deleteMovie(movie.id)}
+                        okText="Có"
+                        cancelText="Không"
+                    >
+                        <Button danger icon={<DeleteOutlined />} />
+                    </Popconfirm>
+                </div>
+            ),
+            className: 'text-center`',
+        },
+    ];
+
+
 
     const handlePageChange = (page: number) => setCurrentPage(page);
 
