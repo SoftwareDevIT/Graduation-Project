7ec9cc1532d24970eb293b85733bfe640519810e
meomeo24dev <meomeo24dev@gmail.com>
2024-12-18 17:29:42 +0700

    update:cinemalist and cityform

--- Client/src/component/Header/Hearder.tsx
+++ Client/src/component/Header/Hearder.tsx
@@ -7,6 +7,7 @@ import instance from "../../server";
 import { useCountryContext } from "../../Context/CountriesContext";
 import { Modal } from "antd"; 
 import { Voucher } from "../../interface/Vouchers";
+import CityForm from "../CityForm/CityForm";
 
 const Header = () => {
   const [isHeaderLeftVisible, setHeaderLeftVisible] = useState(false);
@@ -106,6 +107,13 @@ const Header = () => {
     navigate(`/cinema/${cinemaId}`); // Chuyển hướng tới trang rạp và truyền cinemaId
     console.log("idrap:",cinemaId)
   };
+  const [isFormVisible, setIsFormVisible] = useState(false); // Trạng thái hiển thị form
+
+  const toggleFormVisibility = () => {
+    console.log('Toggle form visibility');
+    setIsFormVisible(!isFormVisible);
+  };
+  
   
 
   return (
@@ -304,19 +312,23 @@ const Header = () => {
   />
 </div>
 
-            <Link to="/map" className="icon-link">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="25"
-                height="25"
-                fill="currentColor"
-                className="bi bi-geo-alt"
-                viewBox="0 0 16 16"
-              >
-                <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
-                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-              </svg>
-            </Link>
+            {/* Biểu tượng để hiển thị CityForm */}
+      <Link to="#" onClick={toggleFormVisibility} className="icon-link">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="25"
+          height="25"
+          fill="currentColor"
+          className="bi bi-geo-alt"
+          viewBox="0 0 16 16"
+        >
+          <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
+          <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+        </svg>
+      </Link>
+
+      {/* Hiển thị form nếu isFormVisible là true */}
+      {isFormVisible && <CityForm isVisible={isFormVisible} onClose={toggleFormVisibility} />}
             <Link to="/sp" className="icon-link">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
